976efede16323b3d7c32800e85024662d39ff3e1
Serhan Baker <serhanbaker@gmail.com>
2014-11-15 11:40:25 +0200

    time optimization on balanced-binary-tree

--- balanced-binary-tree/Solution.java
+++ balanced-binary-tree/Solution.java
@@ -8,18 +8,20 @@
  * }
  */
 public class Solution {
-    public static boolean isBalanced(TreeNode root){
+    public static boolean isBalanced(TreeNode root) {
         if (root == null)
             return true;
-        int heightDiff = getHeight(root.left) - getHeight(root.right);
-        if (Math.abs(heightDiff) > 1)
-            return false;
-        else
-            return isBalanced(root.left) && isBalanced(root.right);
+        return getHeight(root) > -1;
     }
-    public static int getHeight(TreeNode root){
+    
+    public static int getHeight(TreeNode root) {
         if (root == null)
             return 0;
-        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if (Math.abs(leftHeight - rightHeight) > 1 || leftHeight < 0 || rightHeight < 0)
+            return -1;
+        else
+            return Math.max(leftHeight, rightHeight) + 1;
     }
 }
